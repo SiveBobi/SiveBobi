5eee997d82cf72301f44a77ee8bab86b0d741f15
SiveBobi <sivebobi1@gmial.com>
2025-02-17 01:41:13 +0200

    This is my first push of my "Hello world" application.

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#Author Sive Bobi
+class HelloWorld:
+    def __init__(self):
+        self.message = "Hello, World!"
+
+    def display_message(self):
+        print(self.message)
+
+# Create an object of HelloWorld class
+hello = HelloWorld()
+
+# Call the display_message method
+hello.display_message()
